20c89b36d44abdb3a14f6a2b8048ebc5b02a0881
Krishesh shrestha <shrestha.krishesh@gmail.com>
2020-07-06 22:00:21 +0545

    update

--- user_account/admin.py
+++ user_account/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Branch, Tag_for_contacts
+
 # Register your models here.
+admin.site.register(Branch)
+admin.site.register(Tag_for_contacts)
--- user_account/models.py
+++ user_account/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Branch(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='')
+    address = models.CharField(max_length=100, blank=True, default='')
+
+    def __str__(self):
+        return self.name
+
+
+class Tag_for_contacts(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='')
+
+    def __str__(self):
+        return self.name
